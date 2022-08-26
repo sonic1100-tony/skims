61c532c9ed75709c814dd20d8c9b0a17aa943f9f
sonic1100-tony <sonic1100@naver.com>
2022-08-26 02:04:18 +0000

    jenkins upload

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -4,3 +4,8 @@ output "skims-ec2-bastion" {
   description = "instance_id"
   value = aws_instance.skims-ec2-bastion.id
 }
+
+output "skims-ec2-jenkins" {
+  description = "instance_id"
+  value = aws_instance.skims-ec2-jenkins.id
+}
