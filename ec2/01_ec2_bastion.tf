45323ac4e7b83aa6999f8af2039f77435c3245dc
sonic1100-tony <sonic1100@naver.com>
2022-08-29 14:47:19 +0000

    ec2 comment out

--- ec2/01_ec2_bastion.tf
+++ ec2/01_ec2_bastion.tf
@@ -1,79 +1,79 @@
-data "terraform_remote_state" "network" {
-  backend = "s3"
-  config = {
-    bucket = "skims-s3-tf-state"
-    key    = "skims-network/terraform.tfstate"
-    region = "ap-northeast-2"
-  }
-}
+# data "terraform_remote_state" "network" {
+#   backend = "s3"
+#   config = {
+#     bucket = "skims-s3-tf-state"
+#     key    = "skims-network/terraform.tfstate"
+#     region = "ap-northeast-2"
+#   }
+# }
 
-resource "aws_instance" "skims-ec2-bastion" {
+# resource "aws_instance" "skims-ec2-bastion" {
 
-  ami                         = "ami-0444dd1f48b76275b"
-  associate_public_ip_address = "true"
-  availability_zone           = "ap-northeast-2a"
+#   ami                         = "ami-0444dd1f48b76275b"
+#   associate_public_ip_address = "true"
+#   availability_zone           = "ap-northeast-2a"
 
-  # iam_instance_profile        = aws_iam_instance_profile.skims-ec2-instance-profile.name
-  key_name                    = "sk-rds-key"
+#   # iam_instance_profile        = aws_iam_instance_profile.skims-ec2-instance-profile.name
+#   key_name                    = "sk-rds-key"
 
-  capacity_reservation_specification {
-    capacity_reservation_preference = "open"
-  }
+#   capacity_reservation_specification {
+#     capacity_reservation_preference = "open"
+#   }
 
-  credit_specification {
-    cpu_credits = "standard"
-  }
+#   credit_specification {
+#     cpu_credits = "standard"
+#   }
 
-  disable_api_termination = "false"
-  ebs_optimized           = "false"
+#   disable_api_termination = "false"
+#   ebs_optimized           = "false"
 
-  enclave_options {
-    enabled = "false"
-  }
+#   enclave_options {
+#     enabled = "false"
+#   }
 
-  get_password_data                    = "false"
-  hibernation                          = "false"
+#   get_password_data                    = "false"
+#   hibernation                          = "false"
 
-  instance_initiated_shutdown_behavior = "stop"
-  instance_type                        = "t2.micro"
-  ipv6_address_count                   = "0"
-  metadata_options {
-    http_endpoint               = "enabled"
-    http_put_response_hop_limit = "1"
-    http_tokens                 = "optional"
-  }
+#   instance_initiated_shutdown_behavior = "stop"
+#   instance_type                        = "t2.micro"
+#   ipv6_address_count                   = "0"
+#   metadata_options {
+#     http_endpoint               = "enabled"
+#     http_put_response_hop_limit = "1"
+#     http_tokens                 = "optional"
+#   }
 
-  monitoring = "false"
+#   monitoring = "false"
 
-  root_block_device {
-    delete_on_termination = "true"
-    encrypted             = "false"
+#   root_block_device {
+#     delete_on_termination = "true"
+#     encrypted             = "false"
 
-    tags = {
-      Name        = "skims-ec2-bastion-ebs"
-    }
+#     tags = {
+#       Name        = "skims-ec2-bastion-ebs"
+#     }
 
-    volume_size = "50"
-    volume_type = "gp2"
-  }
+#     volume_size = "50"
+#     volume_type = "gp2"
+#   }
 
-  source_dest_check = "true"
+#   source_dest_check = "true"
 
 
-  tags = {
-    Name        = "skims-ec2-bastion"
-  }
+#   tags = {
+#     Name        = "skims-ec2-bastion"
+#   }
 
-  tags_all = {
-    Name        = "skims-ec2-bastion"
-  }
+#   tags_all = {
+#     Name        = "skims-ec2-bastion"
+#   }
 
-  tenancy       = "default"
+#   tenancy       = "default"
   
-  #vpc_id        = data.terraform_remote_state.network.outputs.aws_vpc.vpc_skims_vpc_id
-  subnet_id     = data.terraform_remote_state.network.outputs.subnet_skims_public-subnet01_id
-  vpc_security_group_ids = [data.terraform_remote_state.network.outputs.skims-sg-bastion_id]
+#   #vpc_id        = data.terraform_remote_state.network.outputs.aws_vpc.vpc_skims_vpc_id
+#   subnet_id     = data.terraform_remote_state.network.outputs.subnet_skims_public-subnet01_id
+#   vpc_security_group_ids = [data.terraform_remote_state.network.outputs.skims-sg-bastion_id]
 
-}
+# }
 
 
--- ec2/02_ec2_jenkins.tf
+++ ec2/02_ec2_jenkins.tf
@@ -1,69 +1,69 @@
-resource "aws_instance" "skims-ec2-jenkins" {
+# resource "aws_instance" "skims-ec2-jenkins" {
 
-  ami                         = "ami-0444dd1f48b76275b"
-  associate_public_ip_address = "false"
-  availability_zone           = "ap-northeast-2a"
+#   ami                         = "ami-0444dd1f48b76275b"
+#   associate_public_ip_address = "false"
+#   availability_zone           = "ap-northeast-2a"
 
-  # iam_instance_profile        = aws_iam_instance_profile.skims-ec2-instance-profile.name
-  key_name                    = "sk-rds-key"
+#   # iam_instance_profile        = aws_iam_instance_profile.skims-ec2-instance-profile.name
+#   key_name                    = "sk-rds-key"
 
-  capacity_reservation_specification {
-    capacity_reservation_preference = "open"
-  }
+#   capacity_reservation_specification {
+#     capacity_reservation_preference = "open"
+#   }
 
-  credit_specification {
-    cpu_credits = "standard"
-  }
+#   credit_specification {
+#     cpu_credits = "standard"
+#   }
 
-  disable_api_termination = "false"
-  ebs_optimized           = "false"
+#   disable_api_termination = "false"
+#   ebs_optimized           = "false"
 
-  enclave_options {
-    enabled = "false"
-  }
+#   enclave_options {
+#     enabled = "false"
+#   }
 
-  get_password_data                    = "false"
-  hibernation                          = "false"
+#   get_password_data                    = "false"
+#   hibernation                          = "false"
 
-  instance_initiated_shutdown_behavior = "stop"
-  instance_type                        = "t3a.medium"
-  ipv6_address_count                   = "0"
-  metadata_options {
-    http_endpoint               = "enabled"
-    http_put_response_hop_limit = "1"
-    http_tokens                 = "optional"
-  }
+#   instance_initiated_shutdown_behavior = "stop"
+#   instance_type                        = "t3a.medium"
+#   ipv6_address_count                   = "0"
+#   metadata_options {
+#     http_endpoint               = "enabled"
+#     http_put_response_hop_limit = "1"
+#     http_tokens                 = "optional"
+#   }
 
-  monitoring = "false"
+#   monitoring = "false"
 
-  root_block_device {
-    delete_on_termination = "true"
-    encrypted             = "false"
+#   root_block_device {
+#     delete_on_termination = "true"
+#     encrypted             = "false"
 
-    tags = {
-      Name        = "skims-ec2-jenkins-ebs"
-    }
+#     tags = {
+#       Name        = "skims-ec2-jenkins-ebs"
+#     }
 
-    volume_size = "50"
-    volume_type = "gp2"
-  }
+#     volume_size = "50"
+#     volume_type = "gp2"
+#   }
 
-  source_dest_check = "true"
+#   source_dest_check = "true"
 
 
-  tags = {
-    Name        = "skims-ec2-jenkins"
-  }
+#   tags = {
+#     Name        = "skims-ec2-jenkins"
+#   }
 
-  tags_all = {
-    Name        = "skims-ec2-jenkins"
-  }
+#   tags_all = {
+#     Name        = "skims-ec2-jenkins"
+#   }
 
-  tenancy       = "default"
+#   tenancy       = "default"
   
-  #vpc_id        = data.terraform_remote_state.network.outputs.aws_vpc.vpc_skims_vpc_id
-  subnet_id     = data.terraform_remote_state.network.outputs.subnet_skims_private_subnet01_id
-  vpc_security_group_ids = [data.terraform_remote_state.network.outputs.skims-sg-rds-jenkins_id]
+#   #vpc_id        = data.terraform_remote_state.network.outputs.aws_vpc.vpc_skims_vpc_id
+#   subnet_id     = data.terraform_remote_state.network.outputs.subnet_skims_private_subnet01_id
+#   vpc_security_group_ids = [data.terraform_remote_state.network.outputs.skims-sg-rds-jenkins_id]
 
-}
+# }
 
